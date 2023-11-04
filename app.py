ae9ca66cf7b576c4a31e68f85e9f9bc400eec0bb
Mohammad  Mohsin <mohd.mohsin3637@gmail.com>
2023-11-05 03:05:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -165,7 +165,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 
-def run():
+def run(a,b):
     teams = ['Sunrisers Hyderabad', 'Mumbai Indians', 'Royal Challengers Bangalore', 'Kolkata Knight Riders', 'Kings XI Punjab', 'Chennai Super Kings', 'Rajasthan Royals', 'Delhi Capitals']
     cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi', 'Chandigarh', 'Jaipur', 'Chennai', 'Cape Town', 'Port Elizabeth', 'Durban', 'Centurion', 'East London', 'Johannesburg', 'Kimberley', 'Bloemfontein', 'Ahmedabad', 'Cuttack', 'Nagpur', 'Dharamsala', 'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi', 'Sharjah', 'Mohali', 'Bengaluru']
 
